a214c01feecd46ff3f2a0f731bdb5eda3c042f4e
Tito-Rollis <ouvalres@gmail.com>
2023-05-04 17:43:01 +0700

    initialized

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <h1>Home</h1>,
+    },
+    {
+        path: '/products/:id',
+        element: <h1>Category</h1>,
+    },
+    {
+        path: '/product/:id',
+        element: <h1>Product Detail</h1>,
+    },
+]);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <RouterProvider router={router}></RouterProvider>
+        </div>
+    );
 }
 
 export default App;
