eb87c5e0b226390621feb5f0743643762450c2a6
Tito-Rollis <ouvalres@gmail.com>
2023-05-05 09:52:19 +0700

    setup React Router Dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,41 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider, Route, Outlet } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { Product } from './pages/Product';
+import { Products } from './pages/Products';
+import { Navbar } from './components/Navbar';
+import { Footer } from './components/Footer';
+
+const Layout = () => {
+    return (
+        <div className="app">
+            <Navbar />
+            <Outlet />
+            <Footer />
+        </div>
+    );
+};
 
 const router = createBrowserRouter([
     {
         path: '/',
-        element: <h1>Home</h1>,
-    },
-    {
-        path: '/products/:id',
-        element: <h1>Category</h1>,
-    },
-    {
-        path: '/product/:id',
-        element: <h1>Product Detail</h1>,
+        element: <Layout />,
+        children: [
+            {
+                path: '/',
+                element: <Home />,
+            },
+            {
+                path: '/products/:id',
+                element: <Products />,
+            },
+            {
+                path: '/product/:id',
+                element: <Product />,
+            },
+        ],
     },
 ]);
 
